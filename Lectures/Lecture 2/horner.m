b59d01ba449d8286ae92df2f689d60598fa108e6
Abhigyan Roy <108815183+abhx7@users.noreply.github.com>
2024-11-30 17:20:40 +0530

    tried suggesstion of using radd to check error

--- Lectures/Lecture 2/horner.m
+++ Lectures/Lecture 2/horner.m
@@ -2,10 +2,17 @@
 % for evaluating polynomials
 a=[ 1 2 3 4 5 6 7 8 9 10 ];
 n=length(a) -1 ;
-z=1;
+z=-1;
 b=a(1);
 % Note index shift for a
 for i=1:n
     b=a(i+1)+ z*b;
+end
+p=b
+
+b=a(1);
+%compare error
+for i=1:n
+    b=radd(a(i+1),z*b,1);
 end
 p=b
